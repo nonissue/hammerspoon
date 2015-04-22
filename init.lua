7a7891cf16a5e01f4aaed5c78bd5b0ae4f5d8eda
nonissue <andy@nonissue.org>
2015-04-22 17:51:50 -0600

    changed reload to only reload when init.lua is changed, no longer for any file in directory. could just figure out how to ignore .git .gitignore

--- init.lua
+++ init.lua
@@ -9,5 +9,5 @@ end)
 function reload_config(files)
    hs.reload()
 end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/init.lua", reload_config):start()
 hs.alert.show("Config loaded")
