f76124d9b525d1e3d97b7cdb71aefd4e8444c943
nonissue <andy@nonissue.org>
2016-12-21 23:40:49 -0600

    Cleaned up burnrate

--- plugins/battery/burnrate.lua
+++ plugins/battery/burnrate.lua
@@ -14,21 +14,6 @@ mod.config = {
   allScreens = false,
 }
 
--- local burnRateMenu = hs.menubar.new()
---
--- function BurnRate:new()
---     -- test should be local, but just messing with this for now
---     test = hs.chooser.new(function(input) print(hs.inspect(input)) end)
---         :rows(3)
---     test:choices(chooserChoices)
---     test:width(20)
---     test:subTextColor({red=0, green=0, blue=0, alpha=0.4})
---     -- test:bgDark(true)
---     hs.hotkey.bind({"cmd", "alt", "ctrl"}, 'd', function() test:show() end)
---     notify('burnrate loaded', 'loaded!')
---     -- print(test:query())
--- end
-
 function mod.init()
   logger.i('Enablng burnrate plugin')
   hs.alert('enabling burrnate plugin')
