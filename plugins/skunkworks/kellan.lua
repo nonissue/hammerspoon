bf8af1d5231885c8ca2787a414572500ff20a525
Andrew Williams <andy@nonissue.org>
2017-04-05 16:45:04 -0600

    removed uncommented line

--- plugins/skunkworks/kellan.lua
+++ plugins/skunkworks/kellan.lua
@@ -2,8 +2,6 @@ local mod = {}
 
 local supermash = {"cmd", "alt", "ctrl", "shift"}
 
--- hs.hotkey.bind(alt, 'space', hs.grid.maximizeWindow)
-
 hs.hotkey.bind(supermash, 'right', hs.itunes.next)
 hs.hotkey.bind(supermash, 'left', hs.itunes.previous)
 
