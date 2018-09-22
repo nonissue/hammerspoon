11c0b4ca4ad83909e832f632401afda391a4434a
Andrew Williams <andy@nonissue.org>
2018-09-21 21:03:35 -0600

    Working chooser with simple table

--- Spoons/Crib.spoon/init.lua
+++ Spoons/Crib.spoon/init.lua
@@ -1,6 +1,8 @@
 --- === Crib ===
 ---
 -- Crib
+-- https://github.com/dharmapoudel/hammerspoon-config
+-- https://github.com/scottcs/dot_hammerspoon/tree/master/cheatsheets
 
 local obj = {}
 obj.__index = obj
@@ -12,78 +14,104 @@ obj.author = "andy williams <andy@nonissue.org>"
 obj.homepzage = "https://github.com/nonissue"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 -- end Metadata
- 
+
 -- init logger
-obj.logger = hs.logger.new('Crib', 'verbose')
+obj.logger = hs.logger.new("Crib", "verbose")
 
 obj.chooser = nil
 
 local chooserTable = {
-    {["id"] = 1, ["text"] = "tmux", subText="subtext"},
-    {["id"] = 1, ["text"] = "vim", subText="subtext"},
+    {["id"] = 1, ["text"] = "tmux", subText = "subtext"},
+    {["id"] = 1, ["text"] = "vim", subText = "subtext"},
 }
+
+local cribsheet = {
+    tmux = "tmux shortcuts",
+    nvim = "nvim shortcuts",
+}
+
+obj.defaultHotkeys = {
+    showChooser =              {{"ctrl", "alt", "cmd"},                     "O"},
+}
+
+function obj:bindHotkeys(keys)
+    assert(keys['showChooser'], "Hotkey variable is 'showChooser'")
+
+    hs.hotkey.bindSpec(
+        keys["showChooser"],
+        function()
+            self.chooser:show()
+        end
+    )
+end
 
 function obj:chooserCallback(choice)
     -- if not choicez then focusLastFocused(); return end
-    if choice['id'] == 1 then
-      -- seems to have fixed the binding problem [FIXED?]  z-
-        return choice['id']
-    elseif choice['id'] == 2 then
-      obj:bust(choice['text'])
-    elseif choice['id'] == 3 then
-      obj:bust(choice['text'])
-    elseif choice['id'] == 4 then
-      obj:bust(choice['text'])
-    elseif choice['id'] == 5 then
-      obj:bust(choice['text'])
+    -- why not just index a table?
+    -- if cribsheet[choice.text] then
+    local crib = cribsheet[choice.text]
+    if not crib then
+        hs.alert("cribsheet error")
+        return
+        -- return choice["id"]
     else
-      local URL = self.chooser:query()
-    --   obj:createCustom(URL)
+        hs.alert(crib)
+        return
+        --   obj:createCustom(URL)
     end
 end
 
 --- PaywallBuster:chooser()
 --- Method
---- 
+---
 ---
 --- Parameters:
 ---  * None
 ---
 --- Returns:
 ---  * The - object
-function obj:chooser()
-    self.chooser = hs.chooser.new(
-      function(choice)
-        if not (choice) then
-          print(self.chooser:query())
-          self.chooser:hide()
-        else
-          self:chooserCallback(choice)
+function obj:init()
+    self.chooser =
+        hs.chooser.new(
+        function(choice)
+            if not (choice) then
+                print(self.chooser:query())
+                self.chooser:hide()
+            else
+                self:chooserCallback(choice)
+            end
         end
-      end
     )
-      
+
     self.chooser:choices(chooserTable)
     self.chooser:rows(#chooserTable)
-      
-    self.chooser:queryChangedCallback(function(query)
-      if query == '' then
-        self.chooser:choices(chooserTable)
-      else
-        local choices = {
-          {["id"] = 0, ["text"] = "Custom", subText="Custom search method title"},
-        }
-        self.chooser:choices(choices)
-      end
-    end)
+
+    self.chooser:queryChangedCallback(
+        function(query)
+            if query == "" then
+                self.chooser:choices(chooserTable)
+            else
+                local choices = {
+                    {["id"] = 0, ["text"] = "Custom", subText = "Custom search method title"}
+                }
+                self.chooser:choices(choices)
+            end
+        end
+    )
     self.chooser:width(20)
-    self.chooser:bgDark(false)
-  
+    self.chooser:bgDark(true)
+
     return self
 end
 
-function obj:init()
-    return obj
+-- function obj:init()
+
+--     return obj
+-- end
+
+function obj:show()
+    self.chooser:show()
+    return
 end
 
 function obj:start()
