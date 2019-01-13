a8c815e329bb479f59f6631eb83e29232bae2a5d
nonissue <andy@nonissue.org>
2019-01-12 18:56:31 -0700

    Updated formatting, removed cruft, added new comments for spoons

--- init.lua
+++ init.lua
@@ -7,37 +7,34 @@
 -- If you have concerns (about my sanity or anything else) feel free to
 -- email me at the above address
 ------------------------------------------------------------------------------
+
 -- load basic modules / utils first
 -- that don't need to be spoons
 package.path = package.path .. ";lib/?.lua"
 styles = require("styles")
 utils = require("utilities")
 
-
 -- bind our alert style to default alert style
 for k, v in pairs(styles.alert_default) do
     hs.alert.defaultStyle[k] = v
 end
 
+-- set logger level
 hs.logger.defaultLogLevel = "error"
 
-hs.console.darkMode(true)
-if hs.console.darkMode() then
-    hs.console.outputBackgroundColor{ white = 0.1, alpha = 0.9}
-    hs.console.consoleCommandColor{ white = 1 }
-    hs.console.alpha(1)
-else
-    hs.console.outputBackgroundColor{ white = 1, alpha = 0.9}
-    hs.console.consoleCommandColor{ white = 0.1 }
-    hs.console.alpha(1)
-end
+-- hs.console.darkMode(true)
+-- if hs.console.darkMode() then
+--     hs.console.outputBackgroundColor{ white = 0.1, alpha = 0.9}
+--     hs.console.consoleCommandColor{ white = 1 }
+--     hs.console.alpha(1)
+-- else
+--     hs.console.outputBackgroundColor{ white = 1, alpha = 0.9}
+--     hs.console.consoleCommandColor{ white = 0.1 }
+--     hs.console.alpha(1)
+-- end
 
 hostname = hs.host.localizedName()
 hs_config_dir = os.getenv("HOME") .. "/.hammerspoon/"
-
--- recall hist
-hs.shutdownCallback = function() hs.settings.set('history', hs.console.getHistory()) end
-hs.console.setHistory(hs.settings.get('history'))
 
 -- a MUST / disables window movement jank
 hs.window.animationDuration = 0
@@ -57,7 +54,6 @@ local alt = {"alt"}
 
 -- load scratch stuff
 package.path = package.path .. ";scratch/?.lua"
--- egpu = require("egpu")
 
 -- TODO: Let user enable/display spoons as desired.
 -- i feel like these two should be spoons
@@ -65,16 +61,28 @@ package.path = package.path .. ";scratch/?.lua"
 -- TODO: Move the below to spoons
 hs_reload = require("hammerspoon_config_reload")
 hs_reload.init()
--- burnrate = require("burnrate")
--- burnrate.init()
 
--- Load Spoon from https://github.com/jasonrudolph/ControlEscape.spoon
--- Doesn't do what I wanted (fix issues capslock issues with AEKII)
+------------------------------------------------------------------------------
+--                              START OF SPOONS                             --
+------------------------------------------------------------------------------
+-- ControlEscape.spoon / https://github.com/jasonrudolph/ControlEscape.spoon
+------------------------------------------------------------------------------
+-- I wanted this to resolve my issues with my locking capslock key on my
+-- AEKII m3501, but I don't think it does
+-- It does replace Karabiner Elements for me though, which is nice!
+------------------------------------------------------------------------------
 hs.loadSpoon('ControlEscape'):start()
 
+------------------------------------------------------------------------------
+-- SystemContexts.spoon / by me
+------------------------------------------------------------------------------
 hs.loadSpoon("SystemContexts")
 
+------------------------------------------------------------------------------
+-- SafariKeys.spoon / by me
+------------------------------------------------------------------------------
 hs.loadSpoon("SafariKeys")
+
 local safariHotkeys = {
     tabToNewWin = {mash, "T"},
     mailToSelf = {mash, "U"},
@@ -83,32 +91,61 @@ local safariHotkeys = {
     cycleUserAgent = {mash, "7"}
 }
 
--- TODO: use default hotkeys
-spoon.SafariKeys:bindHotkeys(safariHotkeys)
+spoon.SafariKeys:bindHotkeys(safariHotkeys) -- TODO: use default hotkeys
 
+------------------------------------------------------------------------------
+-- PaywallBuster.spoon / by me
+------------------------------------------------------------------------------
+-- Ultimately this probably isn't necessary, but I do occasionally use it
+------------------------------------------------------------------------------
 hs.loadSpoon("PaywallBuster")
--- TODO: use default hotkeys
-hs.hotkey.bind(mash, "B", function() spoon.PaywallBuster:show() end)
+hs.hotkey.bind(mash, "B", function() spoon.PaywallBuster:show() end) -- TODO: use default hotkeys
 
+------------------------------------------------------------------------------
+-- Zzz.spoon / by me
+------------------------------------------------------------------------------
+-- Sleep timer, puts computer to sleep after an interval
+-- Shows a countdown in the menubar
+-- Can be triggered from menubar
+-- Menubar also provides snooze/shorten functions
+-- There is also a modal to let users enter custom times
+------------------------------------------------------------------------------
 hs.loadSpoon("Zzz")
 spoon.Zzz:init()
--- TODO: use default hotkeys
-hs.hotkey.bind(mash, "S", function() spoon.Zzz.chooser:show() end)
+hs.hotkey.bind(mash, "S", function() spoon.Zzz.chooser:show() end) -- TODO: use default hotkeys
 
+------------------------------------------------------------------------------
+-- Resolute.spoon / by me
+------------------------------------------------------------------------------
+-- Menubar item + modal for quickly changing display resolution
+-- Currently, you have to specify the choices manually
+-- May change that in future
+------------------------------------------------------------------------------
 hs.loadSpoon("Resolute")
 spoon.Resolute:init()
--- TODO: use default hotkeys
-hs.hotkey.bind(mash, "L", function() spoon.Resolute:show() end)
+hs.hotkey.bind(mash, "L", function() spoon.Resolute:show() end) -- TODO: use default hotkeys
 
+------------------------------------------------------------------------------
+-- Fenestra.spoon / by me
+------------------------------------------------------------------------------
+-- My window management stuff
+-- Resize active windows, move stuff between monitors, etc
+------------------------------------------------------------------------------
 hs.loadSpoon("Fenestra")
-spoon.Fenestra:bindHotkeys(spoon.Fenestra.defaultHotkeys)
+spoon.Fenestra:bindHotkeys(spoon.Fenestra.defaultHotkeys) 
 
+------------------------------------------------------------------------------
+-- Crib.spoon / by me
+------------------------------------------------------------------------------
+-- WIP, not finished at all
+------------------------------------------------------------------------------
 hs.loadSpoon("Crib")
 spoon.Crib:bindHotkeys(spoon.Crib.defaultHotkeys)
 
--- hs.loadSpoon("eGPU")
--- spoon.eGPU:start()
---- end of spoons loading
+------------------------------------------------------------------------------
+--                                END OF SPOONS                             --
+------------------------------------------------------------------------------
+
 
 -- random stuff
 -- TODO: move these to chooser?
