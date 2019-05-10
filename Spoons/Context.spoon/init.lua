7929508d1fa93e03a347af0910348be053d23857
aw <andy@nonissue.org>
2019-05-09 18:21:21 -0600

    Renamed SystemContexts -> Context, added menubar icon for Context.

--- Spoons/Context.spoon/init.lua
+++ Spoons/Context.spoon/init.lua
@@ -1,7 +1,7 @@
 local obj = {}
 obj.__index = obj
 
-obj.name = "SystemContexts"
+obj.name = "Context"
 obj.version = "1.0"
 obj.author = "andy williams <andy@nonissue.org>"
 obj.homepage = "https://github.com/nonissue"
@@ -9,13 +9,25 @@ obj.license = "MIT - https://opensource.org/licenses/MIT"
 
 obj.logger = hs.logger.new("SystemContexts")
 obj.hotkeyShow = nil
+
 obj.wifiWatcher = nil
+obj.screenWatcher = nil
 obj.cafWatcher = nil
+
+obj.menubar = nil
+obj.menuIcon = "@"
+obj.menu = {}
+obj.location = nil
+obj.docked = nil
+
 obj.currentSSID = nil
+obj.currentScreens = nil
+obj.lastNumberOfScreens = #hs.screen.allScreens()
+
 -- move to key value store
 local homeSSIDs = {"BROMEGA", "ComfortInn Plus", "1614 Apple II"}
 
-local function has_value (tab, val)
+local function has_value(tab, val)
     for index, value in ipairs(tab) do
         if value == val then
             return true
@@ -28,8 +40,7 @@ end
 function obj.ssidChangedCallback()
     local newSSID = hs.wifi.currentNetwork()
 
-    if (has_value(homeSSIDs, newSSID)) and
-        (not has_value(homeSSIDs, obj.currentSSID)) then
+    if (has_value(homeSSIDs, newSSID)) and (not has_value(homeSSIDs, obj.currentSSID)) then
         -- we are at home!
         obj.logger.i("@home")
         obj.homeArrived()
@@ -48,32 +59,27 @@ function obj.ssidChangedCallback()
 end
 
 function obj.homeArrived()
-  -- Should really have device specific settings (desktop vs laptop)
-  -- requires modified sudoers file.
-  -- For Example!
-  -- IN /etc/sudoers.d/power_mgmt (sudo visudo -f /etc/sudoers.d/power_mgmt)
-  -- andrewwilliams ALL=(root) NOPASSWD: /usr/bin/pmset *
+    -- Should really have device specific settings (desktop vs laptop)
+    -- requires modified sudoers file.
+    -- For Example!
+    -- IN /etc/sudoers.d/power_mgmt (sudo visudo -f /etc/sudoers.d/power_mgmt)
+    -- andrewwilliams ALL=(root) NOPASSWD: /usr/bin/pmset *
     os.execute("sudo pmset -b displaysleep 5 sleep 10")
     os.execute("sudo pmset -c displaysleep 5 sleep 10")
     hs.audiodevice.defaultOutputDevice():setMuted(false)
-    hs.notify.show("@home", "", "")
-    hs.alert(" ☛ ⌂ ", 3)
+    -- hs.notify.show("@home", "", "")
+    hs.alert(" ☛ ⌂ ", 1)
+    obj.location = "@home"
 end
 
 -- sets displaysleep to lowervalue
 -- eventually should unmount disks and perform other functions?
 function obj.homeDeparted()
     hs.audiodevice.defaultOutputDevice():setMuted(true)
-    hs.alert("~(☛ ⌂)", 3)
+    hs.alert("~(☛ ⌂)", 1)
     os.execute("sudo pmset -a displaysleep 1 sleep 5")
-end
 
-function obj.initWifiWatcher()
-    if not obj.currentNetwork then
-        obj.ssidChangedCallback()
-    end
-
-    obj.wifiWatcher = hs.wifi.watcher.new(obj.ssidChangedCallback)
+    obj.location = "@away"
 end
 
 -- this catches situations where we get somewhere, computer wakes from sleep,
@@ -82,10 +88,6 @@ function obj.muteOnWake(eventType)
     if (eventType == hs.caffeinate.watcher.systemDidWake and (not has_value(homeSSIDs, hs.wifi.currentNetwork()))) then
         obj.homeDeparted()
     end
-end
-
-function obj.initCafWatcher()
-    obj.cafWatcher = hs.caffeinate.watcher.new(obj.muteOnWake)
 end
 
 function obj.moveDockLeft()
@@ -97,17 +99,14 @@ function obj.moveDockLeft()
     )
 end
 
-  function obj.moveDockDown()
+function obj.moveDockDown()
     hs.applescript.applescript(
-      [[
+        [[
         tell application "System Events" to set the autohide of the dock preferences to false
         tell application "System Events" to set the screen edge of the dock preferences to bottom
-      ]])
+      ]]
+    )
 end
-
-obj.lastNumberOfScreens = #hs.screen.allScreens()
-obj.currentScreens = hs.screen.allScreens()
-obj.currentScreens = nil
 
 function obj.checkAndEject(target)
     target = "/Volumes/" .. target
@@ -118,45 +117,97 @@ function obj.checkAndEject(target)
     end
 end
 
-function obj.screenWatcher()
+function obj.screenWatcherCallback()
     local newNumberOfScreens = #hs.screen.allScreens()
 
-    if #hs.screen.allScreens() == obj.lastNumberOfScreens and obj.currentScreens then
+    -- if #hs.screen.allScreens() == obj.lastNumberOfScreens and obj.currentScreens then
+    if #hs.screen.allScreens() == obj.lastNumberOfScreens and obj.docked and obj.location then
         -- handle unnecessary/redundant screenWatcher callbacks
-        hs.alert("SW:" .. obj.currentScreens, 3)
+        hs.alert("SW: no change", 1)
     elseif hs.screen.find("Cinema HD") then -- wat. somehow this changed? 18-11-02: it is now 69489832, was 69489838
         -- Changed above line to use "Cinema HD" as display ID was not reliable?
         -- if we have a different amount of displays and one of them is
         -- our cinema display, we are @desk
-        hs.alert("@desk")
-        obj.currentScreens = "@desk"
+        hs.alert("SW: desk", 1)
+        obj.docked = "docked"
         obj.moveDockDown()
     elseif #hs.screen.allScreens() == 1 and hs.screen.find("Color LCD") and obj.currentScreens == "@desk" then
-        hs.alert("@undocking", 3)
-        obj.currentScreens = "@mobile"
+        hs.alert("SW: undocking", 1)
+        obj.docked = "mobile"
+
         obj.moveDockLeft()
         obj.checkAndEject("ExternalSSD")
         obj.checkAndEject("Win-Stuff")
     elseif #hs.screen.allScreens() == 1 and hs.screen.find("Color LCD") then
-        -- hs.notify.show("@mobile", "", "")
-        obj.currentScreens = "@mobile"
+        hs.alert("SW: mobile", 1)
+        obj.docked = "mobile"
+
         obj:moveDockLeft()
     else
-        hs.alert("ERROR: Unhandled screenWatcher case")
-        obj.currentState = "@error"
+        hs.alert("ERROR: Unhandled screenWatcher case", 5)
+        obj.docked = "error"
+        obj.location = "error"
+
+        obj.logger.e("SW Error!")
     end
 
     obj.lastNumberOfScreens = newNumberOfScreens
 end
 
-obj.screenWatcher()
-hs.screen.watcher.new(obj.screenWatcher):start()
+function obj.initWifiWatcher()
+    if not obj.currentNetwork then
+        obj.ssidChangedCallback()
+    end
+
+    obj.wifiWatcher = hs.wifi.watcher.new(obj.ssidChangedCallback)
+end
+
+function obj.initCafWatcher()
+    obj.cafWatcher = hs.caffeinate.watcher.new(obj.muteOnWake)
+end
+
+function obj.initScreenWatcher()
+    obj.screenWatcherCallback()
+    obj.screenWatcher = hs.screen.watcher.new(obj.screenWatcherCallback)
+end
 
 function obj:init()
+    if self.menubar then
+        self.menubar:delete()
+    end
+
+    obj.menubar = hs.menubar.new():setTitle(obj.menuIcon)
+    --[[
+        submenu
+            - current location (@home, ~@home)
+            - docked? (@desk, @mobile)
+            -
+    ]]
+
     obj.initWifiWatcher()
     obj.initCafWatcher()
-    obj.wifiWatcher:start()
-    obj.cafWatcher:start()
+    obj.initScreenWatcher()
+
+
+    obj.menu = {
+        {
+            title = hs.styledtext.new(obj.location),
+            fn = function()
+                hs.alert("location clicked")
+            end
+        },
+        {
+            title = hs.styledtext.new(obj.docked),
+            fn = function()
+                hs.alert("docked clicked")
+            end
+        }
+    }
+    -- table.insert(
+    --     obj.menu,
+    -- )
+
+    obj.menubar:setMenu(obj.menu)
 
     return self
 end
@@ -164,27 +215,34 @@ end
 -- start watchers
 function obj:start()
     obj.logger.i("-- Starting Contexts")
-  if self.hotkeyShow then
-      self.hotkeyShow:enable()
-  end
+    if self.hotkeyShow then
+        self.hotkeyShow:enable()
+    end
 
-  obj.initWifiWatcher()
-  obj.wifiWatcher:start()
-  obj.cafWatcher:start()
-  return self
+    obj:init()
+    --   obj.initWifiWatcher()
+
+    obj.wifiWatcher:start()
+    obj.cafWatcher:start()
+    obj.screenWatcher:start()
+    return self
 end
 
 -- stop watchers
 function obj:stop()
     obj.logger.df("-- Stopping Contexts")
-  if self.hotkeyShow then
-      self.hotkeyShow:disable()
-  end
+    if self.hotkeyShow then
+        self.hotkeyShow:disable()
+    end
 
-  obj.wifiWatcher:stop()
-  obj.cafWatcher:stop()
-  obj.currentSSID = nil
-  return self
+    obj.menubar:delete()
+
+    obj.wifiWatcher:stop()
+    obj.cafWatcher:stop()
+    obj.screenWatcher:stop()
+    obj.currentSSID = nil
+
+    return self
 end
 
 return obj
