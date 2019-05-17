8a3590a6c6d1ef4a6affa594ca0e1ab917f27d5b
aw <andy@nonissue.org>
2019-05-17 17:49:15 -0600

    Added some proper comments, fixed method order

--- Spoons/Context.spoon/init.lua
+++ Spoons/Context.spoon/init.lua
@@ -13,7 +13,6 @@
     -- homeSSIDs
 ]]
 
-
 local obj = {}
 obj.__index = obj
 
@@ -42,8 +41,13 @@ obj.currentSSID = nil
 obj.currentScreens = nil
 obj.lastNumberOfScreens = #hs.screen.allScreens()
 
+-- spoon options
+obj.shownInMenu = false
+
+-- Spoon config options
 -- moved to key/val store
 local homeSSIDs = hs.settings.get("homeSSIDs")
+
 
 -- exists in utilies library, but not accessible from spoon
 local function has_value(tab, val)
@@ -60,6 +64,104 @@ local function atHome(SSID)
     return has_value(homeSSIDs, SSID)
 end
 
+--- Context.moveDockLeft()
+--- Method
+---
+--- Parameters:
+--- * None
+---
+--- Returns:
+---  * None
+function obj.moveDockLeft()
+    hs.applescript.applescript(
+        [[
+            tell application "System Events" to set the autohide of the dock preferences to true
+            tell application "System Events" to set the screen edge of the dock preferences to left
+        ]]
+    )
+end
+
+--- Context.moveDockDown()
+--- Method
+---
+--- Parameters:
+--- * None
+---
+--- Returns:
+---  * None
+function obj.moveDockDown()
+    hs.applescript.applescript(
+        [[
+            tell application "System Events" to set the autohide of the dock preferences to false
+            tell application "System Events" to set the screen edge of the dock preferences to bottom
+      ]]
+    )
+end
+
+--- Context.checkAndEject()
+--- Method
+---
+--- Parameters:
+--- * target - a string containing the name of the drive we wish to eject
+---
+--- Returns:
+---  * None
+function obj.checkAndEject(target)
+    target = "/Volumes/" .. target
+    if hs.fs.volume.eject(target) then
+        hs.alert.show("SUCCESS: Ejected " .. target, 3)
+    else
+        hs.alert.show("ERROR: Unable to eject " .. target, 3)
+    end
+end
+
+
+--- Context.homeArrived()
+--- Method
+---
+--- Parameters:
+--- * None
+---
+--- Returns:
+---  * None
+function obj.homeArrived()
+    -- Should really have device specific settings (desktop vs laptop)
+    -- requires modified sudoers file.
+    -- For Example!
+    -- IN /etc/sudoers.d/power_mgmt (sudo visudo -f /etc/sudoers.d/power_mgmt)
+    -- andrewwilliams ALL=(root) NOPASSWD: /usr/bin/pmset *
+    os.execute("sudo pmset -b displaysleep 2 sleep 10")
+    os.execute("sudo pmset -c displaysleep 5 sleep 15")
+    hs.audiodevice.defaultOutputDevice():setMuted(false)
+
+    hs.alert(" ☛ ⌂ ", 3)
+    obj.location = "@home"
+end
+
+--- Context.homeDeparted()
+--- Method
+---
+--- Parameters:
+--- * None
+---
+--- Returns:
+---  * None
+function obj.homeDeparted()
+    hs.audiodevice.defaultOutputDevice():setMuted(true)
+    hs.alert("~(☛ ⌂)", 1)
+    os.execute("sudo pmset -a displaysleep 1 sleep 5")
+
+    obj.location = "@away"
+end
+
+--- Context.ssidChangedCallback()
+--- Method
+---
+--- Parameters:
+--- * None
+---
+--- Returns:
+---  * None
 function obj.ssidChangedCallback()
     local newSSID = hs.wifi.currentNetwork()
 
@@ -82,32 +184,14 @@ function obj.ssidChangedCallback()
     obj.currentSSID = newSSID
 end
 
-function obj.homeArrived()
-    -- Should really have device specific settings (desktop vs laptop)
-    -- requires modified sudoers file.
-    -- For Example!
-    -- IN /etc/sudoers.d/power_mgmt (sudo visudo -f /etc/sudoers.d/power_mgmt)
-    -- andrewwilliams ALL=(root) NOPASSWD: /usr/bin/pmset *
-    os.execute("sudo pmset -b displaysleep 2 sleep 10")
-    os.execute("sudo pmset -c displaysleep 5 sleep 15")
-    hs.audiodevice.defaultOutputDevice():setMuted(false)
-
-    hs.alert(" ☛ ⌂ ", 3)
-    obj.location = "@home"
-end
-
--- sets displaysleep to lowervalue
--- eventually should unmount disks and perform other functions?
-function obj.homeDeparted()
-    hs.audiodevice.defaultOutputDevice():setMuted(true)
-    hs.alert("~(☛ ⌂)", 1)
-    os.execute("sudo pmset -a displaysleep 1 sleep 5")
-
-    obj.location = "@away"
-end
-
--- this catches situations where we get somewhere, computer wakes from sleep,
--- but doesn't connect to a network automatically. I still want things set
+--- Context.cafChangedCallback()
+--- Method
+---
+--- Parameters:
+--- * eventType - a number which represents the ID of the eventType
+---
+--- Returns:
+---  * None
 function obj.cafChangedCallback(eventType)
     -- handle both?:
         -- hs.caffeinate.watcher.screensDidWake
@@ -120,38 +204,19 @@ function obj.cafChangedCallback(eventType)
     elseif (eventType == didWake) and (atHome(obj.currentSSID)) then
         obj.logger.i("[CW] Woke from sleep, @home")
         obj.homeArrived()
-    elseif (eventType ~= didWake) then
-        obj.logger.i("[CW] nonWakeEvent: " .. eventType)
+    -- elseif (eventType ~= didWake) then
+    --     obj.logger.i("[CW] nonWakeEvent: " .. eventType)
     end
 end
 
-function obj.moveDockLeft()
-    hs.applescript.applescript(
-        [[
-            tell application "System Events" to set the autohide of the dock preferences to true
-            tell application "System Events" to set the screen edge of the dock preferences to left
-        ]]
-    )
-end
-
-function obj.moveDockDown()
-    hs.applescript.applescript(
-        [[
-            tell application "System Events" to set the autohide of the dock preferences to false
-            tell application "System Events" to set the screen edge of the dock preferences to bottom
-      ]]
-    )
-end
-
-function obj.checkAndEject(target)
-    target = "/Volumes/" .. target
-    if hs.fs.volume.eject(target) then
-        hs.alert.show("SUCCESS: Ejected " .. target, 3)
-    else
-        hs.alert.show("ERROR: Unable to eject " .. target, 3)
-    end
-end
-
+--- Context.screenWatcherCallback()
+--- Method
+---
+--- Parameters:
+--- * None
+---
+--- Returns:
+---  * None
 function obj.screenWatcherCallback()
     local newNumberOfScreens = #hs.screen.allScreens()
 
@@ -196,6 +261,14 @@ function obj.screenWatcherCallback()
     obj.lastNumberOfScreens = newNumberOfScreens
 end
 
+--- Context.initWifiWatcher()
+--- Method
+---
+--- Parameters:
+--- * None
+---
+--- Returns:
+---  * None
 function obj.initWifiWatcher()
     -- if location hasn't been set,
     -- run the callback once to populate it
@@ -206,10 +279,28 @@ function obj.initWifiWatcher()
     obj.wifiWatcher = hs.wifi.watcher.new(obj.ssidChangedCallback)
 end
 
+--- Context.initCafWatcher()
+--- Method
+---
+--- Parameters:
+--- * None
+---
+--- Returns:
+---  * None
 function obj.initCafWatcher()
     obj.cafWatcher = hs.caffeinate.watcher.new(obj.cafChangedCallback)
 end
 
+--- Context.initScreenWatcher()
+--- Method
+---
+--- Parameters:
+---  * options - An optional table containing spoon configuration options
+---     showMenu - boolean which indicates whether menubar item is shown
+---     animate - boolean which indicates whether ui mode toggle is animated
+---
+--- Returns:
+---  * None
 function obj.initScreenWatcher()
     -- if docked hasn't been set,
     -- run the callback once to populate it
@@ -220,66 +311,96 @@ function obj.initScreenWatcher()
     obj.screenWatcher = hs.screen.watcher.new(obj.screenWatcherCallback)
 end
 
+
+--- Context:init()
+--- Method
+---
+--- Parameters:
+---  * options - An optional table containing spoon configuration options
+---     showMenu - boolean which indicates whether menubar item is shown
+---     animate - boolean which indicates whether ui mode toggle is animated
+---
+--- Returns:
+---  * None
 function obj:init()
     if self.menubar then
         self.menubar:delete()
     end
 
-    obj.menubar = hs.menubar.new():setTitle(obj.menuIcon)
-    --[[
-        submenu
-            - current location (@home, ~@home)
-            - docked? (@desk, @mobile)
-            -
-    ]]
-
     obj.initWifiWatcher()
     obj.initCafWatcher()
     obj.initScreenWatcher()
 
-    obj.menu = {
-        {
-            title = hs.styledtext.new(obj.location),
-            fn = function()
-                hs.alert("location clicked")
-            end
-        },
-        {
-            title = hs.styledtext.new(obj.docked),
-            fn = function()
-                hs.alert("docked clicked")
-            end
-        }
-    }
-
-    obj.menubar:setMenu(obj.menu)
-
     return self
 end
 
--- start watchers
-function obj:start()
+--- Context:start()
+--- Method
+---
+--- Parameters:
+---  * options - An optional table containing spoon configuration options
+---     showMenu - boolean which indicates whether menubar item is shown
+---     animate - boolean which indicates whether ui mode toggle is animated
+---
+--- Returns:
+---  * None
+function obj:start(options)
     obj.logger.i("-- Starting Contexts")
     if self.hotkeyShow then
         self.hotkeyShow:enable()
     end
 
-    -- obj:init()
-
     obj.wifiWatcher:start()
     obj.cafWatcher:start()
     obj.screenWatcher:start()
+
+
+
+    if options then
+        obj.shownInMenu = options.showMenu or obj.shownInMenu
+    end
+
+    --create icon on the menu bar and set flag to 'false'
+    if obj.shownInMenu then
+        obj.menu = {
+            {
+                title = hs.styledtext.new(obj.location),
+                fn = function()
+                    hs.alert("location clicked")
+                end
+            },
+            {
+                title = hs.styledtext.new(obj.docked),
+                fn = function()
+                    hs.alert("docked clicked")
+                end
+            }
+        }
+
+        obj.menubar = hs.menubar.new():setTitle(obj.menuIcon)
+        obj.menubar:setMenu(obj.menu)
+    end
+
     return self
 end
 
--- stop watchers
+--- Context:stop()
+--- Method
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
 function obj:stop()
     obj.logger.df("-- Stopping Contexts")
     if self.hotkeyShow then
         self.hotkeyShow:disable()
     end
 
-    obj.menubar:delete()
+    if obj.menubar then
+        obj.menubar:delete()
+    end
 
     obj.wifiWatcher:stop()
     obj.cafWatcher:stop()
