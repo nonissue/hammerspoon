46fdbe1cdb340c2b70e350c5f79d691ce28d15d7
aw <andy@nonissue.org>
2019-05-17 17:01:28 -0600

    Cleaned up AfterDark, added comments, handled screenSize change

--- WIP/AfterDark.spoon/init.lua
+++ WIP/AfterDark.spoon/init.lua
@@ -19,124 +19,16 @@ obj.author = "andy williams <andy@nonissue.org>"
 obj.homepage = "https://github.com/nonissue"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
-obj.logger = hs.logger.new("MenuTest")
+obj.logger = hs.logger.new("AfterDark")
 obj.hotkeyShow = nil
 
 obj.menubar = nil
 obj.menu = {}
 
---Find out screen size. Currently using only the primary screen
-obj.screenSize = hs.screen.primaryScreen()
-
---Returns a hs.geometry rect describing screen's frame in absolute coordinates, including the dock and menu.
--- might be better to do this with canvas?
-obj.shade = hs.drawing.rectangle(obj.screenSize:fullFrame())
-
---- AfterDark.shadeTransparency
---- Variable
---- Set to greater than 1
-obj.shadeTransparency = 1.2
-obj.timer = nil
-
---shade characteristics
---white - the ratio of white to black from 0.0 (completely black) to 1.0 (completely white); default = 0.
---alpha - the color transparency from 0.0 (completely transparent) to 1.0 (completely opaque)
-obj.shade:setFillColor({["blue"]=0.5, ["alpha"] = obj.shadeTransparency })
-obj.shade:setStroke(false):setFill(true)
-
---set to cover the whole screen, all spaces and expose
-obj.shade:bringToFront(false):setBehavior(17)
-
 --- AfterDark.darkModeIsOn
 --- Variable
 --- Flag for Shade status, 'false' means shade off, 'true' means on.
 obj.darkModeIsOn = nil
-
-local lightMode = hs.image.imageFromASCII("1.........2\n" ..
-"...........\n" ..
-"...........\n" ..
-".....b.....\n" ..
-"...........\n" ..
-"...........\n" ..
-"....e.f....\n" ..
-"...........\n" ..
-"...a...c...\n" ..
-"...........\n" ..
-"4.........3",
-{
-{ strokeColor = { red = 1 }, fillColor = { alpha = 0.0 } },
-{ strokeColor = { blue = 1 }, fillColor = { alpha = 0.0 }, shouldClose = false },
-{ strokeColor = { green = 1 } },
-{}
-})
-
-local darkMode = hs.image.imageFromASCII("1.........2\n" ..
-"...........\n" ..
-"...........\n" ..
-".....b.....\n" ..
-"...........\n" ..
-"...........\n" ..
-"....e.f....\n" ..
-"...........\n" ..
-"...a...c...\n" ..
-"...........\n" ..
-"4.........3",
-{
-{ strokeColor = { white = .75 }, fillColor = { alpha = 0.5 } },
-{ strokeColor = { white = .75 }, fillColor = { alpha = 0.0 }, shouldClose = false },
-{ strokeColor = { white = .75 } },
-{}
-})
-
---String containing an ASCII diagram to be rendered as a menu bar icon for when Shade is OFF.
-obj.iconOff = "ASCII:" ..
-". . . . . . . . . . . . . . . . . . . . .\n" ..
-". . . . . . . . . . . . . . . . . . . . .\n" ..
-". . . . . . . . . . . . . . . . . . . . .\n" ..
-". . . 1 # # # # # # # # # # # # # 1 . . .\n" ..
-". . . 4 . . . . . . . . . . . . . 2 . . .\n" ..
-". . . # 5 = = = = = = = = = = = 5 # . . .\n" ..
-". . . # . . . . . . . . . . . . . # . . .\n" ..
-". . . # 6 = = = = = = = = = = = 6 # . . .\n" ..
-". . . # . . . . . . . . . . . . . # . . .\n" ..
-". . . # 7 = = = = = = = = = = = 7 # . . .\n" ..
-". . . # . . . . . . . . . . . . . # . . .\n" ..
-". . . # . . . . . . . . . . . . . # . . .\n" ..
-". . . # . . . . . . . . . . . . . # . . .\n" ..
-". . . # . . . . . . . . . . . . . # . . .\n" ..
-". . . # . . . . . . . . . . . . . # . . .\n" ..
-". . . # . . . . . . . . . . . . . # . . .\n" ..
-". . . 4 . . . . . . . . . . . . . # . . .\n" ..
-". . . 3 # # # # # # # # # # # # 3 2 . . .\n" ..
-". . . . . . . . . . . . . . . . . . . . .\n" ..
-". . . . . . . . . . . . . . . . . . . . .\n" ..
-". . . . . . . . . . . . . . . . . . . . ."
---
-
---String containing an ASCII diagram to be rendered as a menu bar icon for when Shade is ON.
-obj.iconOn = "ASCII:" ..
-  ". . . . . . . . . . . . . . . . . . . . .\n" ..
-  ". . . . . . . . . . . . . . . . . . . . .\n" ..
-  ". . . . . . . . . . . . . . . . . . . . .\n" ..
-  ". . . 1 # # # # # # # # # # # # # 1 . . .\n" ..
-  ". . . 4 . . . . . . . . . . . . . 2 . . .\n" ..
-  ". . . # 5 = = = = = = = = = = = 5 # . . .\n" ..
-  ". . . # . . . . . . . . . . . . . # . . .\n" ..
-  ". . . # 6 = = = = = = = = = = = 6 # . . .\n" ..
-  ". . . # . . . . . . . . . . . . . # . . .\n" ..
-  ". . . # 7 = = = = = = = = = = = 7 # . . .\n" ..
-  ". . . # . . . . . . . . . . . . . # . . .\n" ..
-  ". . . # 8 = = = = = = = = = = = 8 # . . .\n" ..
-  ". . . # . . . . . . . . . . . . . # . . .\n" ..
-  ". . . # 9 = = = = = = = = = = = 9 # . . .\n" ..
-  ". . . # . . . . . . . . . . . . . # . . .\n" ..
-  ". . . # a = = = = = = = = = = = a # . . .\n" ..
-  ". . . 4 . . . . . . . . . . . . . # . . .\n" ..
-  ". . . 3 # # # # # # # # # # # # 3 2 . . .\n" ..
-  ". . . . . . . . . . . . . . . . . . . . .\n" ..
-  ". . . . . . . . . . . . . . . . . . . . .\n" ..
-  ". . . . . . . . . . . . . . . . . . . . ."
---
 
 obj.darkModeScript = [[
     tell application "System Events"
@@ -146,45 +38,133 @@ obj.darkModeScript = [[
     end tell
 ]]
 
+--- AfterDark:createOverlay()
+--- Method
+--- creates overlay that will be used during transition
+--- This function checks the screen resolution in case it has
+--- changed, and generates new rect if it has
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function obj.createOverlay()
+    --In case there is already a overlay on the screen, first hide this one
+    obj.overlay:hide()
+
+    -- Find out screen size. Currently using only the primary screen
+    -- obj.screenSize = hs.screen.primaryScreen():fulleFrame()
+    if not obj.screenSize == hs.screen.primaryScreen():fullFrame() then
+        obj.logger.i("screenSize changed since init, adjusting...")
+        obj.screenSize = hs.screen.primaryScreen():fullFrame()
+    end
+
+    --Returns a hs.geometry rect describing screen's frame in absolute coordinates, including the dock and menu.
+    obj.overlay = hs.drawing.rectangle(obj.screenSize)
+
+    -- use something like the following to create a transitiion:
+    obj.overlay:setFillColor({["alpha"] = obj.overlayTransparency})
+    -- obj.menubar:setTitle("☀︎")
+end
+
+--- AfterDark:toggleDarkMode()
+--- Method
+--- Toggles dark mode while fading a rect that covers the whole screen
+--- from black to completely transparent
+--- This is done because there is sometimes a display with darkMode switch
+--- in apps like safari
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
 function obj.toggleDarkMode()
     if obj.timer then
         hs.alert("Already transitioning!")
         return
     end
+
     hs.alert.closeAll()
-    -- hs.alert("ANIMATING!", 0.5)
-    obj.start()
+
+    -- create our default overlay
+    -- the createOverlay function also adjusts rect size
+    -- if screen dimensions have changed
+    obj.createOverlay()
+    obj.overlay:show()
+
     hs.osascript.applescript(obj.darkModeScript)
-    -- super ugly transition as change is kind of janky
-    -- idea lifted from: https://github.com/Hammerspoon/Spoons/blob/master/Source/FadeLogo.spoon/init.lua
-    -- local timer
-    local origOpacity = obj.shadeTransparency
-    -- local opacity = obj.shadeTransparency + 0.3
+
+    -- Kind of janky screen overlay / transition.
+    -- Idea lifted and mispurposed from:
+    -- https://github.com/Hammerspoon/Spoons/blob/master/Source/FadeLogo.spoon/init.lua
+    local origOpacity = obj.overlayTransparency
     obj.timer = hs.timer.doEvery(
-        1,
+        0.1,
         function()
-            if obj.shadeTransparency > 0.1 then
-                obj.shadeTransparency = obj.shadeTransparency - 0.7
-                obj.shade:setFillColor({["white"]=0.5, ["alpha"] = obj.shadeTransparency })
+            if obj.overlayTransparency > 0.1 then
+                obj.overlayTransparency = obj.overlayTransparency - 0.1
+                obj.overlay:setFillColor({["white"]=0.0, ["alpha"] = obj.overlayTransparency })
             else
                 obj.timer:stop()
                 obj.timer = nil
-                obj.shade:hide()
-                obj.shadeTransparency = origOpacity
+                obj.overlay:hide()
+                obj.overlayTransparency = origOpacity
             end
     end)
-    -- obj.timer = nil
-    -- can use the below function to eject during dev
-    -- so we dont get a rectangle stuck on the screen covering everything lol
+
+    -- In case we want an escape to make sure the rectangle overlay is truly gone
     -- hs.timer.doAfter(1.4, function() hs.alert("Ejecting...") timer:stop() obj:stop() end)
 end
 
+
+--- AfterDark:init()
+--- Method
+--- Initialize our AfterDark spoon
+--- * Deletes any existing menubars
+--- * Deletes any leftover overlays
+--- * Creates a new rect with current screenSize
+--- * Sets rect properties
+--- * Creates menubar item, adds it to menubar
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
 function obj:init()
     obj.logger.i("-- AfterDark initialized")
 
     if self.menubar then
         self.menubar:delete()
     end
+
+    if self.overlay then
+        self.overlay:delete()
+    end
+
+    --Find out screen size. Currently using only the primary screen
+    obj.screenSize = hs.screen.primaryScreen():fullFrame()
+
+    --Returns a hs.geometry rect describing screen's frame in absolute coordinates, including the dock and menu.
+    -- might be better to do this with canvas?
+    obj.overlay = hs.drawing.rectangle(obj.screenSize)
+
+    --- AfterDark.rectTransparency
+    --- Set to greater than 1 so there is a slight delay before overlay starts fading out
+    --- Probably not the best way to do this though
+    obj.overlayTransparency = 1.2
+    obj.timer = nil
+
+    --white - the ratio of white to black from 0.0 (completely black) to 1.0 (completely white); default = 0.
+    --alpha - the color transparency from 0.0 (completely transparent) to 1.0 (completely opaque)
+    --Overlay characteristics
+    obj.overlay:setFillColor({ ["white"] = 0.0, ["alpha"] = obj.overlayTransparency })
+    obj.overlay:setStroke(false):setFill(true)
+
+    --set to cover the whole screen, all spaces and expose
+    obj.overlay:bringToFront(false):setBehavior(17)
 
     --create icon on the menu bar and set flag to 'false'
     self.menubar = hs.menubar.new()
@@ -197,10 +177,8 @@ function obj:init()
     return self
 end
 
-
---- Shade:start()
+--- AfterDark:start()
 --- Method
---- Turn the shade on, darkening the screen
 ---
 --- Parameters:
 ---  * None
@@ -208,25 +186,13 @@ end
 --- Returns:
 ---  * None
 function obj.start()
-    --In case there is already a shade on the screen, first hide this one
-    obj.shade:hide()
 
-    --Find out screen size. Currently using only the primary screen
-    obj.screenSize = hs.screen.primaryScreen()
-    
-    --Returns a hs.geometry rect describing screen's frame in absolute coordinates, including the dock and menu.
-    obj.shade = hs.drawing.rectangle(obj.screenSize:fullFrame())
-
-    -- use something like the following to create a transitiion:
-    obj.shade:setFillColor({["alpha"] = obj.shadeTransparency})
-    obj.shade:show()
-    obj.darkModeIsOn = true
-    obj.menubar:setTitle("☀︎")
+    return
 end
 
-  --- Shade:stop()
+  --- AfterDark:stop()
   --- Method
-  --- Turn the shade off, brightening the screen
+  --- Turn the overlay off, delete any timers
   ---
   --- Parameters:
   ---  * None
@@ -236,33 +202,40 @@ end
 function obj.stop()
     obj.logger.df("-- Stopping AfterDark")
 
-    obj.shade:hide()
+    if obj.overlay then
+        obj.overlay:hide()
+    end
+
+    obj.timer = nil
+
     obj.darkModeIsOn = false
     obj.menubar:setTitle("︎︎︎☀︎")
 end
 
--- -- start watchers
--- function obj:start()
 
---     -- if self.hotkeyShow then
---     --     self.hotkeyShow:enable()
---     -- end
+--- AfterDark.disable()
+--- Method
+--- Stops the spoon and removes it from menubar
+--- Cleans up any variables
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function obj.disable()
+    if obj.menubar then
+        obj.menubar:delete()
+    end
 
---     -- obj:init()
+    if obj.overlay then
+        obj.overlay:delete()
+    end
 
---     return self
--- end
-
--- stop watchers
--- function obj:stop()
---     obj.logger.df("-- Stopping Contexts")
---     if self.hotkeyShow then
---         self.hotkeyShow:disable()
---     end
-
---     obj.menubar:delete()
-
---     return self
--- end
+    obj.overlay = nil
+    obj.menubar = nil
+    obj.screenSize = nil
+    obj.timer = nil
+end
 
 return obj
