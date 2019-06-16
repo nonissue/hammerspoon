a9eab6336456e0e706922e9bb2650212e0020f26
aw <andy@nonissue.org>
2019-06-15 20:31:39 -0600

    added refind-utils

--- WIP/Clippy.spoon/init.lua
+++ WIP/Clippy.spoon/init.lua
@@ -10,11 +10,8 @@ obj.license = "MIT - https.//opensource.org/licenses/MIT"
 
 obj.logger = hs.logger.new("Clippy")
 obj.hotkeyShow = nil
-
-obj.screenshotPath = os.getenv("HOME") .. "/Library/CloudStorage/iCloud Drive/Documents/screenshots/2016mbpr"
+obj.screenshotPath = os.getenv("HOME") .. "/Documents/screenshots/2016mbpr"
 obj.wasCreated = false
--- obj.screenshotWatcher = nil
-
 
 -- clipboard stuff
 
@@ -22,7 +19,6 @@ obj.wasCreated = false
 -- https.//github.com/heptal/dotfiles/blob/9f1277e162a9416b5f8b4094e87e7cd1fc374b18/roles/hammerspoon/files/pasteboard.lua
 -- https.//github.com/search?q=hs.pasteboard+extension%3Alua&type=Code
 -- https.//github.com/ahonn/dotfiles/blob/c5e2f2845924daf970dce48aecbae48e325069a9/hammerspoon/modules/clipboard.lua
-
 
 -- watches for new screenshots in target location and copies them to clipboard
 function obj.imageToClipboard(files, flagTables)
@@ -41,65 +37,52 @@ function obj.imageToClipboard(files, flagTables)
         end
     end
 
-    -- print(i(files),i(flagTables))
+    for y = 1, #files do
+        local file = files[y]
 
-    -- for y = 1, #files do
-    --     local file = files[y]
-    --     print(file)
-    --     print(obj.wasCreated)
-    --     print(file:match( "([^/]+)$" ))
-        
-    -- end
+        -- if our file doesn't end in png, we don't care about it
+        if string.sub(file, -4) == ".png" then
+            print("\nFlagtables. " .. i(flagTables).. "\n")
+            print("\nFiles. " .. i(files) .. "\n")
 
-    local continue = true
-    if continue then 
-        for y = 1, #files do
-            local file = files[y]
+            -- get just the file name without the path
+            local fileName = file:match( "([^/]+)$" )
 
-            -- if our file doesn't end in png, we don't care about it
-            if string.sub(file, -4) == ".png" then
-                print("\nFlagtables. " .. i(flagTables).. "\n")
-                print("\nFiles. " .. i(files) .. "\n")
+            -- encode path properly (regular paths don't work for our UTI)
+            local filePath = hs.http.encodeForQuery("file://" .. file)
+            print("\nfilename. " .. fileName .. "\nfilePath. " .. filePath)
 
-                -- get just the file name without the path
-                local fileName = file:match( "([^/]+)$" )
-
-                -- encode path properly (regular paths don't work for our UTI)
-                local filePath = hs.http.encodeForQuery("file.//" .. file)
-                print("\nfilename. " .. fileName .. "\nfilePath. " .. filePath)
-
-                -- if file doesn't start with prefix set in our macos defaults
-                -- then we don't care about it. when a screenshot is created,
-                -- it seems like a bunch of temporary files with a period prepended
-                -- are created and then removed, which breaks our utility.
-                --
-                -- We also make sure this is a "file creation" event before continuing
-                if string.sub(fileName, 1, 3) == "apw" and obj.wasCreated then
-                    if hs.pasteboard.readDataForUTI("public.file-url") == filePath then
-                        print("Already in clipboard")
-                    else
-                        print("\n   ------------------------\n  Match! Copying to clipboard... \n   ------------------------\n")
-                        hs.pasteboard.writeDataForUTI(nil, "public.file-url", filePath)
-                        -- hs.alert("New Screenshot Copied to Clipboard", 0.75)
-                        hs.notify.new({
-                            title = "Screenshot!",
-                            subtitle = "New screenshot detected",
-                            informativeText = "Screenshot copied to clipboard",
-                            alwaysPresent = true, autoWithdraw = true
-                        }):send()
-
-                        obj.wasCreated = false
-                        -- Don't need to go through rest of files if we have a match
-                        break
-                    end
+            -- if file doesn't start with prefix set in our macos defaults
+            -- then we don't care about it. when a screenshot is created,
+            -- it seems like a bunch of temporary files with a period prepended
+            -- are created and then removed, which breaks our utility.
+            --
+            -- We also make sure this is a "file creation" event before continuing
+            if string.sub(fileName, 1, 3) == "apw" and obj.wasCreated then
+                if hs.pasteboard.readDataForUTI("public.file-url") == filePath then
+                    print("Already in clipboard")
                 else
-                    -- Either a file was modified, deleted or created by macos
-                    print("file watcher called, but file is either temporary, or has been removed")
+                    print("\n   ------------------------\n  Match! Copying to clipboard... \n   ------------------------\n")
+                    hs.pasteboard.writeDataForUTI(nil, "public.file-url", filePath)
+                    -- hs.alert("New Screenshot Copied to Clipboard", 0.75)
+                    hs.notify.new({
+                        title = "Screenshot!",
+                        subtitle = "New screenshot detected",
+                        informativeText = "Screenshot copied to clipboard",
+                        alwaysPresent = true, autoWithdraw = true
+                    }):send()
+
+                    obj.wasCreated = false
+                    -- Don't need to go through rest of files if we have a match
+                    break
                 end
             else
-                -- If the entry in our files list doesn't end in png, we aren't concerned about it
-                print("DEBUG. Target not an image file..." .. file)
+                -- Either a file was modified, deleted or created by macos
+                print("file watcher called, but file is either temporary, or has been removed")
             end
+        else
+            -- If the entry in our files list doesn't end in png, we aren't concerned about it
+            print("DEBUG. Target not an image file..." .. file)
         end
     end
 
@@ -129,11 +112,11 @@ end
 --- Returns.
 ---  * None
 function obj.start()
+    obj.logger.df("-- Starting Clippy")
     obj.screenshotWatcher:start()
 end
   --- Clippy:stop()
   --- Method
-  --- Turn the overlay off, delete any timers
   ---
   --- Parameters.
   ---  * None
