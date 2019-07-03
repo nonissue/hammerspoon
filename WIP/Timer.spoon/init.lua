0f8844ed9f1b2b984e78f52c425848515970ed46
Andy Williams <andy@nonissue.org>
2019-07-02 21:36:26 -0600

    renamed reminders: timers

--- WIP/Timer.spoon/init.lua
+++ WIP/Timer.spoon/init.lua
@@ -1,14 +1,29 @@
+--- === Timer ===
+---
+-- Simple timer with alert
+
+--[[
+Todo:
+- [ ] Simplify tables
+- [ ] Allow custom function to be called at timer end
+- [ ] Interface to allow custom input for timer
+
+
+Call when timer ends?
+timerDone = function(result) print("Callback Result: " .. result) end
+hs.dialog.alert(500, 500, timerDone, "Message", "Informative Text", "Button One", "Button Two", "NSCriticalAlertStyle")
+]]
 
 local obj = {}
 obj.__index = obj
 
-obj.name = "Reminders"
+obj.name = "Timer"
 obj.version = "1.0"
 obj.author = "andy williams <andy@nonissue.org>"
 obj.homepage = "https://github.com/nonissue/hammerspoon"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
-obj.logger = hs.logger.new("Reminders")
+obj.logger = hs.logger.new("Timer")
 
 obj.chooser = nil
 obj.timerEvent = nil
@@ -24,7 +39,7 @@ obj.modifyTimerChoices = {}
 obj.modifyMenuChoices = {}
 
 local timerInterval = 15
-local updateInterval = 5
+-- local updateInterval = 5
 local presetCount = 3
 
 for i = 1, presetCount do
@@ -103,6 +118,13 @@ obj.modifyMenuChoices = {
     },
 }
 
+-- Call when timer ends?
+function obj:timerDoneAlert()
+    -- local timerDone = function(result) print("Callback Result: " .. result) end
+    hs.dialog.blockAlert("Message", "Informative Text", "Snooze", "Okay!", "NSCriticalAlertStyle")
+    -- hs.dialog.alert(500, 500, timerDone, "Message", "Informative Text", "Button One", "Button Two", "NSCriticalAlertStyle") -- luacheck: ignore
+end
+
 -- why not just deal with minutes?
 function obj:formatSeconds(s)
     -- from https://gist.github.com/jesseadams/791673
@@ -118,7 +140,7 @@ function obj:formatSeconds(s)
 end
 
 function obj:setTitleStyled(text)
-    self.reminderMenu:setTitle(
+    self.timerMenu:setTitle(
         hs.styledtext.new(
             text
         )
@@ -161,19 +183,20 @@ end
 
 -- hs.timer interval is in seconds
 function obj:startTimer(timerInMins)
-    self.reminderMenu:returnToMenuBar()
+    self.timerMenu:returnToMenuBar()
     if self.timerEvent then
         self.logger.e("Timer already running?")
         hs.alert("Timer alreadfy started")
     else
         self.logger.df("Timer started!")
         self:updateMenu()
-        self.reminderMenu:setMenu(self.modifyMenuChoices)
+        self.timerMenu:setMenu(self.modifyMenuChoices)
         self.timerEvent = hs.timer.doAfter(
             tonumber(timerInMins) * 60,
             function()
                 self.logger.df("Timer finished, sleeping!")
-                hs.alert("Timer event finished!", 99)
+                hs.alert("Timer event finished!", 5)
+                self:timerDoneAlert()
                 self:deleteTimer()
             end
         )
@@ -184,7 +207,7 @@ function obj:deleteTimer()
     self.timerEvent:stop()
     self.timerEvent = nil
     self:setTitleStyled(self.menuBarIcon)
-    self.reminderMenu:setMenu(self.startMenuChoices)
+    self.timerMenu:setMenu(self.startMenuChoices)
 end
 
 function obj:updateMenu()
@@ -211,16 +234,15 @@ function obj:init()
     -- We check to see if our menu already exists, and if so
     -- we delete it. Then we create a new one from scratch
     obj.logger.i('Initiializing Zzz')
-    if self.reminderMenu then
-        self.reminderMenu:delete()
+    if self.timerMenu then
+        self.timerMenu:delete()
     end
 
-    self.reminderMenu = hs.menubar.new():setMenu(obj.startMenuChoices)
+    self.timerMenu = hs.menubar.new():setMenu(obj.startMenuChoices)
     self:setTitleStyled(self.menuBarIcon)
 
     -- adds a menubar click callback to invoke show/hide chooser
     -- so sleep timer can be set with mouse only
-
     return self
 end
 
