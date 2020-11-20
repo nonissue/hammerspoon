b6887f28d0121d3e33911224e7cfd7097def535e
nonissue <andy@nonissue.org>
2020-11-20 16:59:16 -0700

    feat: add preliminary textinflator functionality

---
+++ _scratch/TextInflator.lua
@@ -0,0 +1,66 @@
+local obj = {}
+obj.__index = obj
+
+-- Metadata
+obj.name = "shortcutz"
+obj.version = "1.0"
+obj.author = "andy williams <andy@nonissue.org>"
+obj.homepage = "https://github.com/nonissue"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+function obj.currentDate()
+    print(os.date("%y/%m/%d"))
+    hs.pasteboard.setContents(choice["text"])
+    hs.eventtap.keyStrokes(choice["text"])
+end
+
+local mash = {"cmd", "alt", "ctrl"}
+
+local inflates = {
+    {
+        ["text"] = "site:reddit.com ",
+        ["subText"] = "sr site reddit search",
+        ["uuid"] = "0001"
+    },
+    {
+        ["text"] = hs.settings.get("context.address"),
+        ["subText"] = "address",
+        ["uuid"] = "0002"
+    },
+    {
+        ["text"] = os.date("%y/%m/%d"),
+        ["subText"] = "current date",
+        ["uuid"] = "0004"
+    }
+}
+
+function obj.chooserCallback(choice)
+    hs.alert(choice["text"])
+    hs.pasteboard.setContents(choice["text"])
+    hs.eventtap.keyStrokes(choice["text"])
+end
+
+obj.chooser =
+    hs.chooser.new(
+    function(choice)
+        if not (choice) then
+            return
+        else
+            obj.chooserCallback(choice)
+        end
+    end
+):rows(#inflates):width(20):choices(inflates):searchSubText(true)
+
+function obj:init()
+    hs.hotkey.bind(
+        mash,
+        "J",
+        function()
+            obj.chooser:show()
+        end
+    )
+
+    return self
+end
+
+return obj
