6075c84b44dd7d35206f80d6382436c59b66dfcf
Andy Williams <andy@nonissue.org>
2021-06-28 23:27:13 -0600

    misc: cleanup

--- init.lua
+++ init.lua
@@ -8,33 +8,19 @@
 -- email me at the above address
 ------------------------------------------------------------------------------
 
--- load basic modules / utils first
 package.path = package.path .. ";_lib/?.lua"
 local styles = require("styles")
 local utils = require("utilities")
 
--- TODO: Let user enable/display spoons as desired.
--- i feel like these two should be spoons
--- because they rely on watchers
--- TODO: Move the below to spoons
 local hs_reload = require("hammerspoon_config_reload")
 hs_reload.init()
 
 -- console customization and UI is located elsewhere, so we import it
 require("console")
 
--- Add WIP Spoons to path
--- package.path = hs.configdir .. "/WIP/?.spoon/init.lua;" .. package.path
-
 -- Add my Spoons to path
 package.path = hs.configdir .. "/_Spoons/?.spoon/init.lua;" .. package.path
-
--- Add WIP files to path
--- package.path = package.path .. ";WIP/?.lua"
-
--- load scratch stuff
 package.path = package.path .. ";_scratch/?.lua"
--- package.path = hs.configDir .. ";_scratch/TextInflator.lua"
 
 -- bind our alert style to default alert style
 for k, v in pairs(styles.alert_default) do
@@ -55,16 +41,10 @@ else
     require("hs.ipc")
 end
 
--- set logger level
-hs.logger.defaultLogLevel = "debug"
-
--- This is great, disables all the superfluous hotkey logging
 require("hs.hotkey").setLogLevel("warning")
-
--- a MUST / disables window movement jank
+hs.logger.defaultLogLevel = "debug"
 hs.window.animationDuration = 0
 
--- aliases/globals
 i = hs.inspect
 fw = hs.window.focusedWindow
 bind = hs.hotkey.bind
@@ -75,7 +55,6 @@ print_t = utils.print_r
 print_r = utils.print_r
 hostname = hs.host.localizedName()
 
--- hotkey groups
 local mash = {"cmd", "alt", "ctrl"}
 
 ------------------------------------------------------------------------------
@@ -89,7 +68,6 @@ local mash = {"cmd", "alt", "ctrl"}
 -- EDIT: maybe check this
 -- https://gist.github.com/zcmarine/f65182fe26b029900792fa0b59f09d7f
 ------------------------------------------------------------------------------
--- hs.loadSpoon('CTRLESC')
 hs.loadSpoon("CTRLESC"):start()
 
 ------------------------------------------------------------------------------
@@ -106,17 +84,16 @@ hs.loadSpoon("CTRLESC"):start()
 -- [Optional] Accepts a boolean which dictates whether the menubar item is shown
 -- Defaults to false if nothing is passed
 ------------------------------------------------------------------------------
--- todo: move this all elsewhere so it's private
-hs.settings.set("homeSSIDs", {"BROMEGA", "ComfortInn VIP", "BROMEGA-5", "1614 Apple II", "RamadaGuest"})
+
 local drives = {"ExternalSSD", "Win-Stuff", "Photos"}
--- display ids apparently change? could be flux changing profile...
--- NVM, use UUIDs
 local display_ids = {mbp = 2077750265, cinema = 69489832, sidecar = 4128829}
 
+hs.settings.set("homeSSIDs", {"BROMEGA", "ComfortInn VIP", "BROMEGA-5", "1614 Apple II", "RamadaGuest"})
 hs.settings.set("context.drives", drives)
 hs.settings.set("context.display_ids", display_ids)
 
 hs.loadSpoon("Context"):start({showMenu = true, display_ids = display_ids, drives = drives})
+
 ------------------------------------------------------------------------------
 -- SafariKeys.spoon / by me
 ------------------------------------------------------------------------------
@@ -156,6 +133,7 @@ spoon.Zzz:bindHotkeys(spoon.Zzz.defaultHotkeys)
 -- Get current TOTP token, copy to clipboard and type in frontmost window
 ------------------------------------------------------------------------------
 hs.loadSpoon("EasyTOTP")
+
 ------------------------------------------------------------------------------
 -- Resolute.spoon / by me
 ------------------------------------------------------------------------------
@@ -177,21 +155,6 @@ hs.loadSpoon("Fenestra")
 spoon.Fenestra:bindHotkeys(spoon.Fenestra.defaultHotkeys)
 
 ------------------------------------------------------------------------------
--- AfterDark.spoon / by me
-------------------------------------------------------------------------------
--- Dark mode toggle in menubar
--- PARAMS:
--- [Optional] Accepts a boolean which dictates whether the menubar item is shown
--- Defaults to false if nothing is passed
---
--- NOTE: 19-11-01 Interesting proof of concept, but I don't really use it
--- So disabled for now
-------------------------------------------------------------------------------
--- hs.loadSpoon("AfterDark"):start({showMenu = true, animate = true})
-
--- hs.loadSpoon("ToggleDarkMode"):start({showMenu = true})
-
-------------------------------------------------------------------------------
 -- Clippy.spoon / by me
 ------------------------------------------------------------------------------
 -- Copy screenshot to clipboard and save to disk
@@ -199,88 +162,8 @@ spoon.Fenestra:bindHotkeys(spoon.Fenestra.defaultHotkeys)
 hs.loadSpoon("Clippy"):start()
 
 ------------------------------------------------------------------------------
--- Crib.spoon / by me
-------------------------------------------------------------------------------
--- WIP, not finished at all
-------------------------------------------------------------------------------
--- hs.loadSpoon("Crib")
--- spoon.Crib:bindHotkeys(spoon.Crib.def aultHotkeys)
-------------------------------------------------------------------------------
-
--- WIP Stuff
--- hs.loadSpoon("PlexMini")
--- require("sql3-test")
--- hs.loadSpoon("QuickAdd")
--- require("callbacks")
--- hs.loadSpoon("SysInfo")
--- spoon.SysInfo:startFor(0.1)
--- hs.loadSpoon("Alarm")
-
--- ht = hs.loadSpoon("HammerText")
--- ht.keywords = {
---     jjem = "andy@nonissue.org",
---     jjsr = "site:reddit.com ",
---     jjname = "Max Rydahl Andersen",
---     jjdate = function()
---         return os.date("%B %d, %Y")
---     end
--- }
--- ht:start()
-
-------------------------------------------------------------------------------
 --                                END OF SPOONS                             --
 ------------------------------------------------------------------------------
 
-------------------------------------------------------------------------------
---                                NONSENSE                                  --
-------------------------------------------------------------------------------
-
--- random
-local emojis = {
-    {
-        ["text"] = "¯\\_(ツ)_/¯",
-        ["subText"] = "Kirby",
-        ["uuid"] = "0001"
-    },
-    {
-        ["text"] = "ᕙ(⇀‸↼‶)ᕗ",
-        ["subText"] = "yay",
-        ["uuid"] = "0002"
-    },
-    {
-        ["text"] = "ლ(ಠ益ಠლ)",
-        ["subText"] = "boo",
-        ["uuid"] = "0003"
-    }
-}
-
-local function emojiChooserCallback(choice)
-    hs.alert(choice["text"])
-    hs.pasteboard.setContents(choice["text"])
-    hs.eventtap.keyStrokes(choice["text"])
-end
-
-local emojiChooser =
-    hs.chooser.new(
-    function(choice)
-        if not (choice) then
-            return
-        else
-            emojiChooserCallback(choice)
-        end
-    end
-):rows(3):width(20):choices(emojis):searchSubText(true)
-
-hs.hotkey.bind(
-    mash,
-    "K",
-    function()
-        emojiChooser:show()
-    end
-)
-
 TextInflator = require("TextInflator")
 TextInflator:init()
-
--- This is kinda cool but buggy AF?
--- float = require("float")
