47c39a81839fc66538e2003856195641cb76b386
Andy Williams <andy@nonissue.org>
2022-01-31 17:04:13 -0700

    fix: Fix TextInflator generating stale date if HS config isn't reloaded

--- _scratch/TextInflator.lua
+++ _scratch/TextInflator.lua
@@ -17,34 +17,38 @@ end
 
 local mash = {"cmd", "alt", "ctrl"}
 
-local inflates = {
-    {
-        ["text"] = "9935c101ab17a66",
-        ["subText"] = "throwaway username",
-        ["uuid"] = "0001"
-    },
-    {
-        ["text"] = "site:reddit.com ",
-        ["subText"] = "sr site reddit search",
-        ["uuid"] = "0002"
-    },
-    {
-        ["text"] = os.date("%y-%m-%d"),
-        ["subText"] = "current date",
-        ["uuid"] = "0003"
-    },
-    {
-        ["text"] = hs.settings.get("context.address"),
-        ["subText"] = "address",
-        ["uuid"] = "0004"
+function obj.generateChoices()
+    obj.currentChoices = {
+        {
+            ["text"] = "9935c101ab17a66",
+            ["subText"] = "throwaway username",
+            ["uuid"] = "0001"
+        },
+        {
+            ["text"] = "site:reddit.com ",
+            ["subText"] = "sr site reddit search",
+            ["uuid"] = "0002"
+        },
+        -- issue: text is
+        {
+            ["text"] = os.date("%y-%m-%d"),
+            -- ["text"] = os.date,
+            ["subText"] = "current date",
+            ["uuid"] = "0003"
+        },
+        {
+            ["text"] = hs.settings.get("context.address"),
+            ["subText"] = "address",
+            ["uuid"] = "0004"
+        }
     }
-}
+end
 
 function obj.copyToClipboard(row)
     if (row ~= nil) then
         -- obj.chooser:hide()
         -- self:hide()
-        local selectedRow = inflates[row]
+        local selectedRow = obj.currentChoices[row]
         hs.alert(selectedRow["text"])
         hs.pasteboard.setContents(selectedRow["text"])
     else
@@ -61,23 +65,37 @@ function obj.chooserCallback(choice)
     hs.eventtap.keyStrokes(choice["text"])
 end
 
-obj.chooser =
-    hs.chooser.new(
-    function(choice)
-        if not (choice) then
-            return
-        else
-            obj.chooserCallback(choice)
+function obj.createChooser()
+    obj.generateChoices()
+
+    return hs.chooser.new(
+        function(choice)
+            if not (choice) then
+                return
+            else
+                obj.chooserCallback(choice)
+            end
         end
-    end
-):rows(#inflates + 1):width(20):choices(inflates):searchSubText(true):rightClickCallback(obj.copyToClipboard)
+    ):rows(#obj.currentChoices + 1):width(20):choices(obj.currentChoices):searchSubText(true):rightClickCallback(
+        obj.copyToClipboard
+    )
+end
 
 function obj:init()
+    obj.chooser = obj.createChooser()
+
     hs.hotkey.bind(
         mash,
         "J",
         function()
             obj.chooser:show()
+        end
+    )
+
+    obj.chooser:showCallback(
+        function()
+            obj.generateChoices()
+            obj.chooser:choices(obj.currentChoices)
         end
     )
 
