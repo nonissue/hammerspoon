c9be03e16ac1c0ad809f11f3cdd273c726b2f7ba
nonissue <andy@nonissue.org>
2025-09-18 19:21:20 -0600

    disable build bot

--- init.lua
+++ init.lua
@@ -272,8 +272,8 @@ TextInflator:init()
 -- Messing around wtih asmagills spoon formatting but it's weird
 -- hs.loadSpoon is easier for me to reason about
 
-hs.loadSpoon("BuildBot")
-spoon.BuildBot:bindHotkeys(spoon.BuildBot.defaultHotkeys)
+-- hs.loadSpoon("BuildBot")
+-- spoon.BuildBot:bindHotkeys(spoon.BuildBot.defaultHotkeys)
 
 
 -- hs.loadSpoon("CMDESC"):start()
